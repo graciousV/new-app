d4fd7837e89379f3a4a5f1f4f0476e10766ff730
Gracious Wanjiru <gwanjiru@viewtech.co.ke>
2025-07-10 11:26:33 +0300

    Fix merge conflict and update App

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Header from './components/header'
-import Button from './components/button'
-import Card from './components/card'
+import Header from './components/Header'
+import Button from './components/Button'
+import Card from './components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
